11903bc309c81ee40e55876d96707dfeca55439f
Kundan10424 <kundankumar10424>
2025-04-01 14:43:01 +0530

    first

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-hsg5.onrender.com",
+        target: "https://ecommerce-production-aa96.up.railway.app/",
         changeOrigin: true,
         secure: false
       }
